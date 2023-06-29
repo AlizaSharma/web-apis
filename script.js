b9616922b533b179a5675150a3ec5332519a3b48
Aliza Sharma <aliza.sharma12@gmail.com>
2023-06-28 19:26:10 -0500

    added questions to js

--- script.js
+++ script.js
@@ -9,24 +9,32 @@ startButton.addEventListener("click",function () {
     questionsPage.classList.remove("hide") 
 })
 
-
 var questions = [
     {
         question: "Commonly used data types DO Not Include:",
-        choices: ["strings","booleans","alerts","numbers"],
+        choices: ["1. strings"," 2. booleans"," 3. alerts"," 4. numbers"],
         answer: "alerts"
     },
     {
-        title: "",
-        choices: ["Me", "Us", "We", "Them"],
-        answer: "Them"
+        question: "The condition in an if / else statement is enclosed with ________.",
+        choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        answer: "parenthesis"
     },
     {
-        title: "Time"
+        question: "Arrays in JavaScript can be used to store ________.",
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "all of the above"
     },
     {
-        title: "Ok"
-    }
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        answer: "curly brackets"
+    },
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["1. JavaScript"," 2. terminal/bash"," 3. for loops"," 4. console.log"],
+    answer: "for loops"
+}
+    
 ]
 
-questionsPage.textContent = questions[1].title
